7e532578e1fb79ca12e7d0466a017966231841a3
Brad Parbs <brad@bradparbs.com>
2022-12-24 10:42:40 -0600

    Update mh-minluck-and-cre.user.js

--- mh-minluck-and-cre.user.js
+++ mh-minluck-and-cre.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ MouseHunt - Minluck & Catch Rate Estimate
-// @version      1.40.3
+// @version      1.41.0
 // @description  View the minluck and catch rate estimate, right on the camp page.
 // @license      MIT
 // @author       bradp
